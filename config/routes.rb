4712d419ebf1cf739be14c0845b7bf00c10b37ad
vagrant <vagrant@localhost.localdomain>
2020-07-15 09:23:27 +0000

    top page作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#top'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
+  resources :users, only: [:index, :show, :edit, :update]
 end
