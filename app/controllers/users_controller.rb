2eccbbf3390f75c90e523eafff57b3551b892a6e
vagrant <vagrant@localhost.localdomain>
2020-07-16 14:35:46 +0000

    [Add] Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all.order(created_at: :asc)
   end
 
   def show
     @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def edit
