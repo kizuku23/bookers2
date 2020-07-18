71e946a501592bc3bcf67fbee3ccaab92cde51d3
vagrant <vagrant@localhost.localdomain>
2020-07-18 04:43:42 +0000

    [Finish] RSpec クリア

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '10f6b8fd6b76101657c1e8e88f826ee15556ee2e56ab83d84d14a8e84af725fbbd790b5dc05b3b3dd829d564f3bc4ad8b5905b8422f04dbb26732071b04c2962'
